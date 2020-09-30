daae76dfb8d1da43862302275a902c38647a34da
Shadab Khan <shadab.2005@gmail.com>
2020-09-30 20:07:30 +0530

    Add connect wallet

--- mintingUI/js/scripts.js
+++ mintingUI/js/scripts.js
@@ -1,69 +1,118 @@
 /*!
-    * Start Bootstrap - Freelancer v6.0.0 (https://startbootstrap.com/themes/freelancer)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
-    */
-    (function($) {
-    "use strict"; // Start of use strict
-  
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: (target.offset().top - 71)
-          }, 1000, "easeInOutExpo");
-          return false;
-        }
+ * Start Bootstrap - Freelancer v6.0.0 (https://startbootstrap.com/themes/freelancer)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
+ */
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 71,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
       }
-    });
-  
-    // Scroll to top button appear
-    $(document).scroll(function() {
-      var scrollDistance = $(this).scrollTop();
-      if (scrollDistance > 100) {
-        $('.scroll-to-top').fadeIn();
-      } else {
-        $('.scroll-to-top').fadeOut();
-      }
-    });
-  
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-      $('.navbar-collapse').collapse('hide');
-    });
-  
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-      target: '#mainNav',
-      offset: 80
-    });
-  
-    // Collapse Navbar
-    var navbarCollapse = function() {
-      if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
-      } else {
-        $("#mainNav").removeClass("navbar-shrink");
-      }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-  
-    // Floating label headings for the contact form
-    $(function() {
-      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-      }).on("focus", ".floating-label-form-group", function() {
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $(".scroll-to-top").fadeIn();
+    } else {
+      $(".scroll-to-top").fadeOut();
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 80,
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Floating label headings for the contact form
+  $(function () {
+    $("body")
+      .on("input propertychange", ".floating-label-form-group", function (e) {
+        $(this).toggleClass(
+          "floating-label-form-group-with-value",
+          !!$(e.target).val()
+        );
+      })
+      .on("focus", ".floating-label-form-group", function () {
         $(this).addClass("floating-label-form-group-with-focus");
-      }).on("blur", ".floating-label-form-group", function() {
+      })
+      .on("blur", ".floating-label-form-group", function () {
         $(this).removeClass("floating-label-form-group-with-focus");
       });
+  });
+
+  // Establish NEAR connection
+  const config = {
+    nodeUrl: "https://rpc.testnet.near.org",
+    deps: {
+      keyStore: new nearApi.keyStores.BrowserLocalStorageKeyStore(),
+    },
+    networkId: "default",
+    contractName: "dev-1601436477182-8079303",
+    walletUrl: "https://wallet.testnet.near.org",
+    helperUrl: "https://helper.testnet.near.org",
+  };
+
+  // open a connection to the NEAR platform
+  (async function () {
+    window.near = await nearApi.connect(config);
+
+    const walletConnection = new nearApi.WalletConnection(near);
+
+    console.log(walletConnection.isSignedIn());
+
+    window.contract = await new nearApi.Contract(
+      walletConnection.account(),
+      "dev-1601436477182-8079303",
+      {
+        viewMethods: ["get_plant"],
+        changeMethods: ["mint_plant"],
+        sender: walletConnection.getAccountId(),
+      }
+    );
+    // ---------------------------------------------------------------------------
+    // here you have access to `near-api-js` and a valid connection object `near`
+    // ---------------------------------------------------------------------------
+    $("#connect-wallet").click(function () {
+      walletConnection.requestSignIn("dev-1601436477182-8079303", "Plantary");
     });
-  
-  })(jQuery); // End of use strict
-  
+  })(window);
+})(jQuery); // End of use strict
