b0697c4aa747a71b4214c18baf6a09be969827dc
Mykle Hansen <mykle@netx.net>
2020-10-15 18:01:43 -0700

    added harvests

--- contracts/rust/src/constants.rs
+++ contracts/rust/src/constants.rs
@@ -26,16 +26,17 @@ pub mod htypes {
     use crate::constants::HarvestType;
     pub const GENERIC: HarvestType= 0;
 }
+
 // prices to harvest
-pub mod hprices [
-    =1, // generic
+pub const HPRICES: [i32; 7] = [
+    -1, // generic
     5, // oracle
     5, // portrait
     -1, // money (can't harvest)
     5,
     5,
     50
-]
+];
 
 // nested array of meta_urls for possible plants!
 // array index == PlantType (an int)
@@ -71,11 +72,29 @@ lazy_static! {
     "https://arweave.net/v63RbTVHhGKr7UNMmwMjBtKepk1I26UB4yxPhJVSkcg",
     "https://arweave.net/hvOKZAw3miEA8BE4VewzH9io4fNsSWyZpGZaSmhr-l8",
     "https://arweave.net/B_c8uZaUFIA8hjLDVr3v4IR6aRT-zzvCaE0cqWgVURc",
+    "https://arweave.net/mGhn0lNxVB6rfon61c9rRioMKL3ZsbjrVJA0qt9St4o",
+    "https://arweave.net/3xMnn8J1ViLX8uHRfDxMpAZS2tSwT7VWrdjDT3fV2xQ",
+    "https://arweave.net/_q0UfS76GMma9PR-XMavRI8ozipY_cmgoi6TFS_eHOg",
+    "https://arweave.net/-Nxk3noWBskl8kbfxhCZFspD7v9lf79iJt1bQ2TCTzw",
+    "https://arweave.net/zXIyOvf6q42eiVnixg6EK_RmFfxlZaFuaQgvs9b6Y8c",
+    "https://arweave.net/u2wER7li2oXgMXfRUs22oERc-XUsn2Ph9yBsZPrvcBc",
+    "https://arweave.net/eQQKfobStzP8dHIzbXYjJCMKQR1owIZ5ljjwX3xvz7I",
         ]);
         map.insert(ptypes::PORTRAIT, vec![
     "https://arweave.net/tmOUL9xwL8LQb_E5kOldLaF0mrZLg9rSMYpoTGgdkU8",
     "https://arweave.net/tvCQax-rq-oDvRdy-QnBp5orrjSP04Y-dNxXC3maTkI",
     "https://arweave.net/CJyoNeeDM_Vco0l4-7y434_pe4hBhWEE9vvh5XqMd4k",
+    "https://arweave.net/hZ3etzVzsaXX6utSldyfvIvp0JUoFISuA72vJpNKa8s",
+    "https://arweave.net/63vqengRMJiiBU-YmVpRH9nDclZB_f3zNVsn0wtcqg4",
+    "https://arweave.net/mTFjapdnAWqWLXOqySLM0cHX2AOcYYWpoSPArBA_suk",
+    "https://arweave.net/eaCk3l8Oi3MqNi7lKMRRC7gR5zRXO9JfbJc80OquHQk",
+    "https://arweave.net/30kVPubXOw6vJce923j6Nv27jWl39AeS4EcpMijCmZA",
+    "https://arweave.net/1xw0bRDaU-CV7hsOGnP51ZWr5_zVk21Qxu8h_jcX-tg",
+    "https://arweave.net/FK4nE9euzIoEx4QOPpocSKDK0wjrPwcxX0cjxx8Km5I",
+    "https://arweave.net/zIU6uG94XnwtTeEzHB3GSikBTtErmE3fgWCZV744tZE",
+    "https://arweave.net/usIqVRzLyFNGENUgeV8c5-zjzEptOJZa23BkUDiU3cU",
+    "https://arweave.net/EDiBwvIYUmT5cmPqbW02HOuFZnHUPoTNX_ri3N2BeTg",
+    "https://arweave.net/LJkU3DnETelIpCdn6l6v-ZDUdY0LzW77G3qNPkRl7cs", // Dennis, by Ilan Katin
         ]);
         map
     };
