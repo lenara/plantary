a005683e63f7e912c36851cd7ea2fa8a9bda15a5
lenara <lenara@verle.com>
2020-10-18 16:38:27 +0200

    Update constants.rs

--- contracts/rust/src/constants.rs
+++ contracts/rust/src/constants.rs
@@ -68,14 +68,17 @@ lazy_static! {
         map.insert(ptypes::ORACLE, vec![
     "https://3bvdryfdm3sswevmvr3poka2ucda5dfqag3bz4td72affctbmaea.arweave.net/2Go44KNm5SsSrKx29ygaoIYOjLABthzyY_6AUophYAg", 
     "https://vwanp7rn32rioq6ofcvglo52sgdrctcfkc4v7uiy7bbimtzijz3q.arweave.net/rYDX_i3eoodDziiqZbu6kYcRTEVQuV_RGPhChk8oTnc",
+    "https://arweave.net/VoJ1Wx6xTflalopLxOuj7TpO8pC0urYB-vLiZ1FxYno",
         ]);
         map.insert(ptypes::PORTRAIT, vec![
     "https://rsigfpny3j3uwohxfeo7tdkdvw6yhaefxt6d3uq7kajtpaqtdfwq.arweave.net/jJBivbjad0s49ykd-Y1Drb2DgIW8_D3SH1ATN4ITGW0",
     "https://arweave.net/fo--Wlh83Ka83zVQqliiwFq_4zbc1H7vrZNlvA_Gkek",
+    "https://arweave.net/1oDuE6UNrNC4Y_aNfhp_Vde_II2ZIFsuRT1hBYbRydc",
         ]);
         map.insert(ptypes::MONEY, vec![
     "https://rj32ukhcq4hdq7nux3rntp5ffdk3ff2kzjcalpy3mc7batjytoza.arweave.net/ineqKOKHDjh9tL7i2b-lKNWyl0rKRAW_G2C-EE04m7I",
     "https://b2zjlf2zplj5we2bdar6p6smu3o6fdu7o7ed23takt63lck6peoq.arweave.net/DrKVl1l609sTQRgj5_pMpt3ijp93yD1uYFT9tYleeR0",
+    "https://arweave.net/q8RPmg2qf6nfE4Gc1at7bqOBuWbSsEtzxvdICb1NYzk",
         ]);
         map
     };
@@ -93,6 +96,7 @@ lazy_static! {
     "https://arweave.net/zXIyOvf6q42eiVnixg6EK_RmFfxlZaFuaQgvs9b6Y8c",
     "https://arweave.net/u2wER7li2oXgMXfRUs22oERc-XUsn2Ph9yBsZPrvcBc",
     "https://arweave.net/eQQKfobStzP8dHIzbXYjJCMKQR1owIZ5ljjwX3xvz7I",
+    "https://arweave.net/eYJ3Ie8K3sVwvXSH5xtXtyi8PizJCzkXK4n7MCCMunE", // Oracle Voucher, by Oculardelusion
         ]);
         map.insert(ptypes::PORTRAIT, vec![
     "https://arweave.net/tmOUL9xwL8LQb_E5kOldLaF0mrZLg9rSMYpoTGgdkU8",
@@ -109,6 +113,7 @@ lazy_static! {
     "https://arweave.net/usIqVRzLyFNGENUgeV8c5-zjzEptOJZa23BkUDiU3cU",
     "https://arweave.net/EDiBwvIYUmT5cmPqbW02HOuFZnHUPoTNX_ri3N2BeTg",
     "https://arweave.net/LJkU3DnETelIpCdn6l6v-ZDUdY0LzW77G3qNPkRl7cs", // Dennis, by Ilan Katin
+    "https://arweave.net/MiV3Xi4qmRjquQJTK3usefWMy62DJ1TCemb0jsY1NBo", // Portrait Voucher, by Oculardelusion
         ]);
         map
     };
